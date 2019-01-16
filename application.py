8ca3567ee281c532e527f0d0fd0e5e8a6187420f
matheusnicolas <matheuspb8@gmail.com>
2019-01-15 21:06:29 -0300

    static and json with data

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, url_for, jsonify
 import os
-
-app = Flask(__name__)
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 json_url = os.path.join(SITE_ROOT, 'static', 'data.json')
 data = json.load(open(json_url))
 
-@app.route("/numbers", method=['GET'])
+app = Flask(__name__)
+
+@app.route("/numbers")
 def index():
     return render_template("index.html", data=data['numbers'])
+
+@app.route("/numbers/json")
+def jsonfy():
+    return jsonify(data)
